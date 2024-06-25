51a9c4317105bd2e94d14fe20990af7ceb284faa
Alex <awu245@gmail.com>
2024-06-25 18:04:38 +0000

    Tues 2ppm

--- src/interstate.ml
+++ src/interstate.ml
@@ -1,4 +1,71 @@
 open! Core
+module City = String
+
+(* We separate out the [Network] module to represent our social network in
+   OCaml types. *)
+module Network = struct
+  (* We can represent our social network graph as a set of connections, where
+     a connection represents a friendship between two people. *)
+  module Highway = struct
+    module T = struct
+      type t = string * City.t list [@@deriving compare, sexp]
+    end
+
+    (* This funky syntax is necessary to implement sets of [Connection.t]s.
+       This is needed to defined our [Network.t] type later. Using this
+       [Comparable.Make] functor also gives us immutable maps, which might
+       come in handy later. *)
+    include Comparable.Make (T)
+
+    let of_string (s : string) =
+      match String.split s ~on:',' with
+      | head :: tail -> Some (head, tail)
+      | _ -> None
+    ;;
+  end
+
+  module Connection = struct
+    module T = struct
+      type t =
+        { highway_name : string
+        ; city_pair : City.t * City.t
+        }
+      [@@deriving compare, sexp]
+    end
+
+    include Comparable.Make (T)
+  end
+
+  type t = Connection.Set.t [@@deriving sexp_of]
+
+  let rec make_all_pairs (cities : string list) : (City.t * City.t) list =
+    let head = List.hd_exn cities in
+    let tail = List.tl_exn cities in
+    let create_pair t = List.map t ~f:(fun (x : string) -> head, x) in
+    if List.length tail = 1
+    then create_pair tail
+    else List.append (create_pair tail) (make_all_pairs tail)
+  ;;
+
+  let of_file input_file =
+    let connections =
+      In_channel.read_lines (File_path.to_string input_file)
+      |> List.concat_map ~f:(fun s ->
+        match Highway.of_string s with
+        | Some (a, b) ->
+          (* Friendships are mutual; a connection between a and b means we
+             should also consider the connection between b and a. *)
+          List.map (make_all_pairs b) ~f:(fun x ->
+            { Connection.T.highway_name = a; city_pair = x })
+        | None ->
+          printf
+            "ERROR: Could not parse line as connection; dropping. %s\n"
+            s;
+          [])
+    in
+    Connection.Set.of_list connections
+  ;;
+end
 
 let load_command =
   let open Command.Let_syntax in
@@ -9,25 +76,30 @@ let load_command =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing interstates and the cities they go through"
       in
       fun () ->
-        ignore (input_file : File_path.t);
-        failwith "TODO"]
+        let network = Network.of_file input_file in
+        (* This special syntax can be used to easily sexp-serialize values
+           (whose types have [sexp_of_t] implemented). *)
+        printf !"%{sexp: Network.t}\n" network]
 ;;
 
 let visualize_command =
   let open Command.Let_syntax in
   Command.basic
     ~summary:
-      "parse a file listing interstates and generate a graph visualizing the highway \
-       network"
+      "parse a file listing interstates and generate a graph visualizing \
+       the highway network"
     [%map_open
       let input_file =
         flag
           "input"
           (required File_path.arg_type)
-          ~doc:"FILE a file listing all interstates and the cities they go through"
+          ~doc:
+            "FILE a file listing all interstates and the cities they go \
+             through"
       and output_file =
         flag
           "output"
